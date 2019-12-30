d0132dc6a4237b13d9d5cb8c910930734ed442c7
Kevin Campbell <59258474+klcampbell6502@users.noreply.github.com>
2019-12-29 19:37:56 -0600

    Update api.py

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restful import Resource, Api
+import os
 
 app = Flask(__name__)
 api = Api(app)
@@ -16,4 +17,4 @@ api.add_resource(ScreenOff,'/screen/off')
 api.add_resource(ScreenOn,'/screen/on')
 
 if __name__ == '__main__':
-    app.run(debug=False,port=5000, host='0.0.0.0')
+    app.run(debug=True,port=5000, host='0.0.0.0')
