f241af93bbb9ac9e67a27cfb14c07a80d879b3a7
Kevin Campbell <59258474+klcampbell6502@users.noreply.github.com>
2019-12-30 09:17:56 -0600

    Update api.py

--- api.py
+++ api.py
@@ -17,4 +17,4 @@ api.add_resource(ScreenOff,'/screen/off')
 api.add_resource(ScreenOn,'/screen/on')
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5000, host='0.0.0.0')
+    app.run(host='0.0.0.0')
